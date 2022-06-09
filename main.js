eed60b1a9a124e34d4cb4076c08d0d37bac022c9
Felipe Alvarenga <felipe.alvarenga01@yahoo.com.br>
2022-06-08 23:27:58 -0300

    "Class 04 Ended"

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
+const botao = document.getElementById('backToTopButton')
+const navigation = document.getElementById('navigation')
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+  showbackToTopButtonOnScroll()
+ showNavOnScroll()
+}
+
+function showbackToTopButtonOnScroll(){
+
+
+  if (scrollY > 550) {
+    botao.classList.add('show')
+  } else {
+    botao.classList.remove('show')
+  }
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
+
+
 
 function openMenu() {
   document.body.classList.add('menu-expanded')
